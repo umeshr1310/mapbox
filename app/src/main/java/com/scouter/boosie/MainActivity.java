dbec57c6915011f0563da93fae67faaeb941b29d
Umesh <r.umeshr84@gmail.com>
2019-01-20 22:19:54 +0530

    Initial commit

---
+++ app/src/main/java/com/scouter/boosie/MainActivity.java
@@ -0,0 +1,200 @@
+package com.scouter.boosie;
+
+import android.annotation.SuppressLint;
+import android.location.Location;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineListener;
+import com.mapbox.android.core.location.LocationEnginePriority;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback , LocationEngineListener,
+        PermissionsListener,MapboxMap.OnMapClickListener {
+
+    private MapView mapView;
+    private MapboxMap map;
+    private PermissionsManager permissionsManager;
+    private LocationEngine locationEngine;
+    private LocationLayerPlugin locationLayerPlugin;
+    private Location originLocation;
+    private Point originPosition, destinationPosition;
+    private Marker destinationMarker;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getResources().getString(R.string.access_token));
+        setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        map = mapboxMap;
+        map.addOnMapClickListener(this);
+        enableLocation();
+    }
+
+    private void enableLocation(){
+        if(PermissionsManager.areLocationPermissionsGranted(this)){
+            //do some stuff
+            initalizeLocationEngine();
+            initializeLocationLayer();
+        }
+        else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+
+    }
+    @SuppressLint("MissingPermission")
+    private void initalizeLocationEngine(){
+        locationEngine = new LocationEngineProvider(this).obtainBestLocationEngineAvailable();
+        locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
+        locationEngine.activate();
+
+         Location lastLocation = locationEngine.getLastLocation();
+        if(lastLocation!= null){
+            originLocation = lastLocation;
+            setCameraPosition(lastLocation);
+        }
+        else {
+            locationEngine.addLocationEngineListener(this);
+        }
+    }
+    @SuppressLint("MissingPermission")
+    private void initializeLocationLayer(){
+        locationLayerPlugin = new LocationLayerPlugin(mapView,map,locationEngine);
+        locationLayerPlugin.setLocationLayerEnabled(true);
+        locationLayerPlugin.setCameraMode(CameraMode.TRACKING);
+        locationLayerPlugin.setRenderMode(RenderMode.NORMAL);
+
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng point) {
+        if(destinationMarker!=null){
+            map.removeMarker(destinationMarker);
+        }
+        destinationMarker = map.addMarker(new MarkerOptions().position(point));
+        destinationPosition = Point.fromLngLat(point.getLongitude(),point.getLatitude());
+        originPosition = Point.fromLngLat(originLocation.getLongitude(),originLocation.getLatitude());
+
+    }
+    private void setCameraPosition(Location location){
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),
+                location.getLongitude()),13.0));
+    }
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onConnected() {
+        locationEngine.requestLocationUpdates();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location!=null){
+            originLocation = location;
+            setCameraPosition(location);
+        }
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+    //present toast or dialog
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if(granted){
+            enableLocation();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode,permissions,grantResults);
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(locationEngine!=null){
+            locationEngine.requestLocationUpdates();
+        }
+        if(locationLayerPlugin!=null){
+            locationLayerPlugin.onStart();
+        }
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(locationEngine!=null){
+            locationEngine.removeLocationUpdates();
+        }
+        if(locationLayerPlugin!=null){
+            locationLayerPlugin.onStop();
+        }
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(locationEngine!=null){
+            locationEngine.deactivate();
+        }
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+
+
+}
